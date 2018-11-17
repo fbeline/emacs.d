bd80512f8dcf732b0d9737c2e16da6d140fa82bf
fbeline <fe.belineb@gmail.com>
2018-11-17 14:04:18 -0200

    company
    configure selet-next and select-previous

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -1,5 +1,8 @@
 (use-package company
   :ensure t
+  :bind (:map company-active-map
+              ("C-n" . company-select-next)
+              ("C-p" . company-select-previous))
   :config
   (global-company-mode))
 
